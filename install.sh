202775c846af93c555996203bb5bdef3678d9145
Egor Larionov <egor.larionov@gmail.com>
2026-09-20 09:16:09 -0700

    Update Oh My Tmux repository URL

--- install.sh
+++ install.sh
@@ -93,7 +93,7 @@ install() {
   printf '✅ Using %s\n' "${TMUX_CONF_LOCAL/#"$HOME"/'~'}" >&2
 
   printf '\n'
-  OH_MY_TMUX_REPOSITORY=${OH_MY_TMUX_REPOSITORY:-https://github.com/gpakosz/.tmux.git}
+  OH_MY_TMUX_REPOSITORY=${OH_MY_TMUX_REPOSITORY:-https://github.com/elrnv/.tmux.git}
   printf '⬇️  Cloning Oh my tmux! repository...\n' >&2
   if ! is_true "$DRY_RUN"; then
     mkdir -p "$(dirname "$OH_MY_TMUX_CLONE_PATH")"
@@ -145,7 +145,7 @@ if [ -p /dev/stdin ]; then
     self() {
       printf '# Oh my tmux!\n'
       printf '# 💛🩷💙🖤❤️🤍\n'
-      printf '# https://github.com/gpakosz/.tmux\n'
+      printf '# https://github.com/elrnv/.tmux\n'
       printf '\n'
 
       declare -f install
